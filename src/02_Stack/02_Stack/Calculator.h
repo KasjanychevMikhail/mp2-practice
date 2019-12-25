c7d234c08409fb7ba3fb9c4fd97f74b6a7a76c3a
KasjanychevMikhail <kasmihan@rambler.ru>
2019-12-25 23:10:09 +0300

    stackfinal

---
+++ src/02_Stack/02_Stack/Calculator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Stack.h"
+#include <string>
+using namespace std;
+
+class Calculator
+{
+private:
+	static bool Prior(char, char);
+	static double Calc(double, double, char);
+public:
+	static string CreatePostForm(const string&);
+	static double Calculate(const string&, const double*, const char*, int);
+	static void GetOper(const string&, double*&, char*&, int&);
+};
---
+++ src/02_Stack/02_Stack/Stack.h
@@ -0,0 +1,94 @@
+#pragma once
+
+template<class ValueType>
+class TStack
+{
+private:
+	int size;
+	ValueType *arr;
+	int top;
+public:
+	TStack(int _size = 10);
+	TStack(const TStack<ValueType>& tmp);
+	~TStack();
+	void Push(ValueType c);
+	ValueType Pop();
+	ValueType Top() const;
+	TStack& operator=(const TStack& tmp);
+	bool IsFull()const;
+	bool IsEmpty()const;
+};
+
+template<class ValueType>
+TStack<ValueType>::TStack(int _size)
+{
+	size = _size;
+	top = -1;
+	arr = new ValueType[size];
+}
+
+template<class ValueType>
+TStack<ValueType>::TStack(const TStack<ValueType>& tmp)
+{
+	size = tmp.size;
+	top = tmp.top;
+	arr = new ValueType[size];
+	for (int i = 0; i < size; i++)
+		arr[i] = tmp.arr[i];
+}
+
+template<class ValueType>
+TStack<ValueType>::~TStack()
+{
+	size = 0;
+	top = -1;
+	delete[] arr;
+}
+
+template<class ValueType>
+void TStack<ValueType>::Push(ValueType c)
+{
+	if (this->IsFull())
+		throw "Full";
+	arr[++top] = c;
+}
+
+template<class ValueType>
+ValueType TStack<ValueType>::Pop()
+{
+	if (this->IsEmpty())
+		throw "Empty";
+	return arr[top--];
+}
+
+template<class ValueType>
+ValueType TStack<ValueType>::Top() const
+{
+	return arr[top];
+}
+
+template<class ValueType>
+TStack<ValueType>& TStack<ValueType>::operator=(const TStack<ValueType>& tmp)
+{
+	size = tmp.size;
+	top = tmp.top;
+	delete[] arr;
+	arr = new ValueType[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = tmp.arr[i];
+	}
+	return *this;
+}
+
+template<class ValueType>
+bool TStack<ValueType>::IsFull() const
+{
+	return (top == size);
+}
+
+template<class ValueType>
+bool TStack<ValueType>::IsEmpty() const
+{
+	return (top == -1);
+}
