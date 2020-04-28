3f7f9efb5f2339a6d9b3401adc00fb8db23dea24
KasjanychevMikhail <kasmihan@rambler.ru>
2020-04-28 11:36:21 +0300

    list4

--- src/04_List/04_List/Main.cpp
+++ src/04_List/04_List/Main.cpp
@@ -6,40 +6,48 @@ using namespace std;
 void main() {
     setlocale(LC_ALL, "Russian");
     int rezh;
-    cout << "Выберите тест" << endl;
-    cout << "1 - Search" << endl << "2 - Push" << endl << "3 - InsertAfter" << endl << "4 - InsertBefore" << endl << "5 - Back" << endl << "6 - Copy" << endl << "7 - Remove" << endl << "0 - Выход" << endl;
-    cin >> rezh;
-    switch (rezh) {
-    case 1: {
-        TestSearch();
-        break;
-    }
-    case 2: {
-        TestPush();
-        break;
-    }
-    case 3: {
-        TestInsertAfter();
-        break;
-    }
-    case 4: {
-        TestInsertBefore();
-        break;
-    }
-    case 5: {
-        TestBack();
-        break;
-    }
-    case 6: {
-        TestCopy();
-        break;
-    }
-    case 7: {
-        TestRemove();
-        break;
-    }
-    case 0: {
-        return;
-    }
-    }
+    do {
+        cout << "Выберите тест" << endl;
+        cout << "1 - Search" << endl << "2 - Push" << endl << "3 - InsertAfter" << endl << "4 - InsertBefore" << endl << "5 - Back" << endl << "6 - Copy" << endl << "7 - Remove" << endl << "0 - Выход" << endl;
+        cin >> rezh;
+        switch (rezh) {
+        case 1: {
+            TestSearch();
+            rezh = -1;
+            break;
+        }
+        case 2: {
+            TestPush();
+            rezh = -1;
+            break;
+        }
+        case 3: {
+            TestInsertAfter();
+            rezh = -1;
+            break;
+        }
+        case 4: {
+            TestInsertBefore();
+            rezh = -1;
+            break;
+        }
+        case 5: {
+            TestBack();
+            rezh = -1;
+            break;
+        }
+        case 6: {
+            TestCopy();
+            rezh = -1;
+            break;
+        }
+        case 7: {
+            TestRemove();
+            rezh = -1;
+            break;
+        }
+        default:
+            break;
+        }
+    } while (rezh != 0);
 }
